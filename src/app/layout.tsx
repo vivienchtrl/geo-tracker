f9ddd9f8ca34b14d6e7c7b0bc798eb4e064e2b77
vivienchtrl <contact@vivienchantrel.com>
2025-12-21 21:46:01 +0200

    added script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Figtree } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import Script from "next/script";
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 const inter = Inter({ subsets: ["latin"] });
@@ -33,6 +34,12 @@ export default function RootLayout({
             <Toaster />
           </div>
         </ThemeProvider>
+
+        <Script
+          src="https://geo-tracker-teal.vercel.app/tracker.js"
+          data-project-id="6699bf7c-b3fb-4853-bdf9-54bebd316a1f"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
