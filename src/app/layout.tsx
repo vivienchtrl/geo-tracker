56a1cd1d1d7cdb26d5ebdab32251636126a06b6a
vivienchtrl <contact@vivienchantrel.com>
2025-12-22 16:21:12 +0200

    delete tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter, Figtree } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
-import Script from "next/script";
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 const inter = Inter({ subsets: ["latin"] });
@@ -35,12 +34,6 @@ export default function RootLayout({
           </div>
         </ThemeProvider>
 
-        {/* Geo Tracker */}
-        <Script
-          src="https://geo-tracker-teal.vercel.app/tracker.js"
-          data-project-id="6699bf7c-b3fb-4853-bdf9-54bebd316a1f"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
